aaf3e35413c830c2d09f6c913d6b862e8fdd9787
Cleidson Goes <jorgegoes4@gmail.com>
2025-10-10 16:57:11 -0300

    Excluindo coluna

--- index.py
+++ index.py
@@ -1,5 +1,11 @@
+"""
+Este módulo lê um arquivo CSV e exibe suas primeiras linhas.
+"""
 import pandas as pd
 
-tabela = pd.read_csv('ClienteBanco.csv')
+tabela = pd.read_csv('ClientesBanco.csv', encoding='iso-8859-1')
+
+"""Excluindo coluna CLIENTNUM"""
+tabela = tabela.drop("CLIENTNUM", axis=1)
 
 print(tabela.head())
